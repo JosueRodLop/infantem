7418e0874f7d896d4d528118b5d86cdca1f8e5b8
Javier Santos <javsanmar5@alum.us.es>
2025-03-06 14:09:02 +0100

    fix: Move questions to separated file Refs #122

--- frontend/app/alergenos/alergenos.tsx
+++ frontend/app/alergenos/alergenos.tsx
@@ -1,15 +1,7 @@
 import { Link } from "expo-router";
 import { useState } from "react";
 import { Text, View, ScrollView, TouchableOpacity, TextInput } from "react-native";
-
-type QuestionType = "boolean" | "options" | "text";
-
-type Question = {
-  id: number;
-  question: string;
-  type: QuestionType;
-  options?: string[];
-};
+import { questions } from "../../hardcoded_data/questionsData";
 
 export default function Page() {
   
@@ -20,68 +12,12 @@ export default function Page() {
 
   // TODO: This must be retrieved from the back. The JSON is temporary
   //const [questions, setQuestions] = useState([]);
-  const questions: Question[] = [
-    {
-      "id": 1,
-      "question": "¿Ha consumido su hijo/a este alimento por primera vez?",
-      "type": "boolean"
-    },
-    {
-      "id": 2,
-      "question": "¿Cuánto tiempo ha pasado desde que consumió el alimento?",
-      "type": "options",
-      "options": [
-        "Menos de 1 hora",
-        "Entre 1 y 3 horas",
-        "Entre 3 y 6 horas",
-        "Más de 6 horas"
-      ]
-    },
-    {
-      "id": 3,
-      "question": "¿Ha presentado alguno de estos síntomas después de consumirlo?",
-      "type": "options",
-      "options": [
-        "Erupción cutánea",
-        "Hinchazón en labios o cara",
-        "Dificultad para respirar",
-        "Vómitos",
-        "Diarrea",
-        "Dolor abdominal",
-        "Otros"
-      ]
-    },
-    {
-      "id": 4,
-      "question": "Si seleccionó 'Otros', describa los síntomas:",
-      "type": "text",
-    },
-    {
-      "id": 5,
-      "question": "¿Ha consumido este alimento antes sin presentar síntomas?",
-      "type": "boolean"
-    },
-    {
-      "id": 6,
-      "question": "¿Tiene algún antecedente familiar de alergias alimentarias?",
-      "type": "boolean"
-    },
-    {
-      "id": 7,
-      "question": "¿Ha sido diagnosticado previamente con alguna alergia alimentaria?",
-      "type": "options",
-      "options": [
-        "Sí, por un médico",
-        "No",
-        "No estoy seguro/a"
-      ]
-    }
-  ];
 
   const handleAnswer = (answer: string) => {
     setAnswers((prev) => ({ ...prev, [questions[currentQuestion].id]: answer }));
     setCurrentQuestion((prev) => prev + 1);
   };
+  console.log(questions);
 
   return (
     <ScrollView contentContainerStyle={gs.container}>
