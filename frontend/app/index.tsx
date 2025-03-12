7c04a0ce298228e8c91189ce1c4d56aad2bf945e
angelalopol03 <Angela Lpez>
2025-03-12 19:52:43 +0100

    feat:Home-Page Style

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,27 +1,65 @@
 import { Link } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground, Image } from "react-native";
 
 export default function Index() {
-  const gs = require("../static/styles/globalStyles"); 
+  const gs = require("../static/styles/globalStyles");
 
   return (
-    <View style={[gs.container, { justifyContent: "center" }]}>
-
-      <View style={[gs.card, {maxWidth: 500}]}> 
-        <Text style={gs.headerText}> Bienvenido a Infantem </Text>
-        <Text style={gs.subHeaderText}>Aquí podrás dar el mejor cuidado a tu bebé.</Text>
+    <ImageBackground 
+      source={require("../static/images/Background.png")}  
+      style={{ flex: 1, width: "100%", height: "100%", justifyContent: "center" }} 
+      imageStyle={{ resizeMode: "cover", opacity: 0.9 }}
+    >
+      <View style={[gs.container, { flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingHorizontal: 20, backgroundColor:"transparent", width: "100%" }]}>
       
-        <View style={{marginTop:20, gap:10}}> 
-          <Link style={[gs.mainButton, {width:"100%", textAlign:"center"}]} href={"/signin"}>
-            <Text style={gs.mainButtonText} >Inicia sesión</Text>
-          </Link>
-          <Link style={[gs.secondaryButton, {width:"100%", textAlign:"center"}]} href={"/recipes"}>
-            <Text style={gs.secondaryButtonText} >Continúa como invitado</Text>
-          </Link>
+      
+        <View style={{ flex: 1, padding: 20 }}>
+        
+        <Text style={{ color: "#1565C0", fontSize: 60, fontWeight: "bold", textAlign: "left" }}>
+          <Text style={{ fontWeight: "bold" ,fontStyle: "italic"}}>INFANTEM</Text>
+          </Text>
+          <Text style={{ color: "#1565C0", fontSize: 36, fontWeight: "bold", textAlign: "left" }}>
+            El mejor cuidado <Text style={{ fontStyle: "italic" }}>para tu bebé</Text>
+          </Text>
+
+          <Text style={{ color: "#1565C0", fontSize: 20, textAlign: "left" }}>
+          <Text style={{ fontStyle: "italic" }}>Recomendaciones Personalizadas</Text>
+          </Text>
+          <Text style={{ color: "#1565C0", fontSize: 20, textAlign: "left" }}>
+          <Text style={{ fontStyle: "italic" }}>Validado por Expertos</Text>
+          </Text>
+          <Text style={{ color: "#1565C0", fontSize: 20, textAlign: "left" }}>
+          <Text style={{ fontStyle: "italic" }}>Contenido actualizado</Text>
+          </Text>
+          <Text style={{ color: "#1565C0", fontSize: 20, textAlign: "left" }}>
+          <Text style={{ fontStyle: "italic" }}>Acceso rápido y sencillo</Text>
+          </Text>
+
+          <View style={{ marginTop: 20, gap: 10 }}>
+            <Link style={[gs.mainButton, { width:150, backgroundColor: "#1565C0", padding: 12, borderRadius: 8, alignItems: "center" }]} href={"/signin"}>
+              <Text style={[gs.mainButtonText, { color: "#FFFFFF", fontSize: 16, fontWeight: "bold" }]}>
+                Inicia sesión
+              </Text>
+            </Link>
+
+            <Link style={[gs.secondaryButton, { width: 250, backgroundColor: "#E3F2FD", padding: 12, borderRadius: 8, borderWidth: 1, borderColor: "#1565C0", alignItems: "center" }]} href={"/recipes"}>
+              <Text style={[gs.secondaryButtonText, { color: "#1565C0", fontSize: 16, fontWeight: "bold" }]}>
+                Continúa como invitado
+              </Text>
+            </Link>
+          </View>
         </View>
 
+        <View style={{ flex: 1 }}>
+          <Image 
+            source={require("../static/images/food.png")}  
+            style={{ width:"100%", resizeMode: "contain",borderTopLeftRadius:200, borderBottomLeftRadius:200 }} 
+          />
+        </View>
+        
+
       </View>
-		</View>
+      
+    </ImageBackground>
   );
 }
-
