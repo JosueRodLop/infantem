2e417dffeb6819277deb498f015df75197df8e92
Miguel Galán Lerate <miggaller@alum.us.es>
2025-03-12 23:40:43 +0100

    feat: add execution profiles

--- src/main/java/com/isppG8/infantem/infantem/InfantemApplication.java
+++ src/main/java/com/isppG8/infantem/infantem/InfantemApplication.java
@@ -3,10 +3,16 @@ package com.isppG8.infantem.infantem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class InfantemApplication {
 
 	public static void main(String[] args) {
+		
+		Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("spring.profiles.active", dotenv.get("SPRING_PROFILES_ACTIVE"));
 		SpringApplication.run(InfantemApplication.class, args);
 	}
 
