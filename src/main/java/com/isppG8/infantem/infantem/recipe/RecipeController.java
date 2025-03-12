00b47eeb69cadbb4f0001456d993b8860e5c2000
josrodlop19 <josrodlop19@alum.us.es>
2025-03-12 01:17:05 +0100

    fix: Change Recipe logic to use current user Refs #52

--- src/main/java/com/isppG8/infantem/infantem/recipe/RecipeController.java
+++ src/main/java/com/isppG8/infantem/infantem/recipe/RecipeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/api/v1/recipes")
 public class RecipeController {
@@ -47,6 +48,7 @@ public class RecipeController {
 
     @PostMapping
     public ResponseEntity<Recipe> createRecipe(@RequestBody Recipe recipe) {
+
         Recipe createdRecipe = recipeService.createRecipe(recipe);
         return ResponseEntity.status(201).body(createdRecipe);
     }
--- src/main/java/com/isppG8/infantem/infantem/recipe/RecipeRepository.java
+++ src/main/java/com/isppG8/infantem/infantem/recipe/RecipeRepository.java
@@ -1,14 +1,15 @@
 package com.isppG8.infantem.infantem.recipe;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
     
     @Query("SELECT r FROM Recipe r WHERE r.minRecommendedAge <= :age AND r.maxRecommendedAge >= :age AND r.user IS NULL")
@@ -16,4 +17,5 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
     @Query("SELECT r FROM Recipe r WHERE r.user.id = :userId")
     List<Recipe> findRecipesByUserId(@Param("userId") Long userId);
+
 }
