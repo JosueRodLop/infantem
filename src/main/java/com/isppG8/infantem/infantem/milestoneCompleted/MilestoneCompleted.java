bed6c80d49a78ac2884648ed08365633cb90f187
Miguel Galán Lerate <miggaller@alum.us.es>
2025-03-17 17:21:13 +0100

    fix: fix * imports

--- src/main/java/com/isppG8/infantem/infantem/milestoneCompleted/MilestoneCompleted.java
+++ src/main/java/com/isppG8/infantem/infantem/milestoneCompleted/MilestoneCompleted.java
@@ -1,15 +1,19 @@
 package com.isppG8.infantem.infantem.milestoneCompleted;
 
-import java.util.*;
-
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.isppG8.infantem.infantem.baby.Baby;
 import com.isppG8.infantem.infantem.milestone.Milestone;
-
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Date;
 
 @Getter
 @Setter
