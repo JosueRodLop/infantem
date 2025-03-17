cc28a9bc64e0865017420cdafb292d33c8273ee1
Luis-Giraldo-Santiago <luisgirsan@gmail.com>
2025-03-17 16:47:35 +0100

    fix: add new function. Refs #179

--- src/main/java/com/isppG8/infantem/infantem/subscription/SubscriptionRepository.java
+++ src/main/java/com/isppG8/infantem/infantem/subscription/SubscriptionRepository.java
@@ -1,14 +1,18 @@
 package com.isppG8.infantem.infantem.subscription;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.isppG8.infantem.infantem.user.User;
+
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
     @Query("SELECT s FROM Subscription s WHERE s.startDate >= :firstDayOfMonth AND s.startDate < :nextMonth")
-    List<Subscription> findSubscriptionsExpiringThisMonth(@Param("firstDayOfMonth") LocalDate firstDayOfMonth, 
-                                                          @Param("nextMonth") LocalDate nextMonth);
+    List<Subscription> findSubscriptionsExpiringThisMonth(@Param("firstDayOfMonth") LocalDate firstDayOfMonth, @Param("nextMonth") LocalDate nextMonth);
+
+    Optional<Subscription> findByUserAndActiveTrue(User user);
 }
