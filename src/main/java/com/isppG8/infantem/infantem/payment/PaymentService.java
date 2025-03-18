00ec160ee19aa0af6a4ed16a6f49315fa23b467f
Luis-Giraldo-Santiago <luisgirsan@gmail.com>
2025-03-18 09:52:20 +0100

    fix: Add CRUD in Service. Refs #179

--- src/main/java/com/isppG8/infantem/infantem/payment/PaymentService.java
+++ src/main/java/com/isppG8/infantem/infantem/payment/PaymentService.java
@@ -3,6 +3,7 @@ package com.isppG8.infantem.infantem.payment;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,28 @@ public class PaymentService {
     public List<Payment> getAllPayments() {
         return paymentRepository.findAll();
     }
+
+    public Optional<Payment> getPaymentById(long id) {
+        return paymentRepository.findById(id);
+    }
+
+    public void create(Payment payment) {
+        paymentRepository.save(payment);
+    }
+
+    public Payment update(long id, Payment payment) {
+        Payment paymentToUpdate = paymentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Payment not found"));
+        paymentToUpdate.setIsDefault(payment.getIsDefault());
+        paymentToUpdate.setPaymentType(payment.getPaymentType());
+        paymentToUpdate.setBillingAgreementId(payment.getBillingAgreementId());
+        paymentToUpdate.setPaypalEmail(payment.getPaypalEmail());
+        return paymentRepository.save(payment);
+    }
+
+    public void delete(long id) {
+        paymentRepository.deleteById(id);
+    }
+    
 
 
 }
