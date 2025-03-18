217dc7930e97ea787329bf93137d7f51a37b9108
Luis-Giraldo-Santiago <luisgirsan@gmail.com>
2025-03-18 09:52:13 +0100

    fix: Add CRUD in Controller. Refs #179

--- src/main/java/com/isppG8/infantem/infantem/payment/PaymentController.java
+++ src/main/java/com/isppG8/infantem/infantem/payment/PaymentController.java
@@ -1,8 +1,14 @@
 package com.isppG8.infantem.infantem.payment;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +23,27 @@ public class PaymentController {
     public List<Payment> getAllPayments() {
         return paymentService.getAllPayments();
     }
+
+    @GetMapping("/{id}")
+    public Optional<Payment> getPaymentById(@PathVariable long id) {
+        return paymentService.getPaymentById(id);
+    }
+
+
+    @PostMapping
+    public void create(@RequestBody Payment payment) {
+        paymentService.create(payment);
+    }
+
+    @PutMapping("/{id}")
+    public Payment update(long id, @RequestBody Payment payment) {
+        return paymentService.update(id, payment);
+    }
+    
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        paymentService.delete(id);
+    }
+
+
 }
