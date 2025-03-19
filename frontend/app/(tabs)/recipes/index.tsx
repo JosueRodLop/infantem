e6c6cc1f022cedf839c3ef0dc18cd9b00029bd93
angelalopol03 <Angela Lpez>
2025-03-19 19:43:38 +0100

    feat: Recepy Frontend (Not Finish)

--- frontend/app/(tabs)/recipes/index.tsx
+++ frontend/app/(tabs)/recipes/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Text, View, TouchableOpacity, TextInput, ScrollView, Image } from "react-native";
+import { Text, View, TouchableOpacity, TextInput, ScrollView, Image,ImageBackground } from "react-native";
 import { Link } from "expo-router";
 import { Recipe } from "../../../types/Recipe";
 // import { getToken } from "../../../utils/jwtStorage"
@@ -154,23 +154,39 @@ export default function Page() {
   };
 
   return (
-    <View style={{ flex: 1 }}>
 
-      <ScrollView contentContainerStyle={[gs.container, { paddingTop: 100, paddingBottom: 100, flexGrow: 1 }]}>
-        <Text style={gs.headerText}>Recetas Recomendadas</Text>
-        <Text style={gs.bodyText}>
-          Encuentre recetas adecuadas en función de la edad y las preferencias de su bebé
+      <ImageBackground
+              source={require("../../../static/images/Background.png")}
+              style={{ flex: 1, width: "100%", height: "100%", justifyContent: "center" }}
+              imageStyle={{ resizeMode: "cover", opacity: 0.9 }}
+      >      
+    <ScrollView contentContainerStyle={[gs.container, { paddingTop: 100, paddingBottom: 100, backgroundColor:"transparent" }]}>        
+    <Image source={require("../../../static/images/Bottle.png")} style={{ position: 'absolute', top: "0%", right: "70%", width: 150, height: 150,transform: [{ rotate: '15deg' }] }} />
+
+        <Text style={{ color: "#1565C0", fontSize: 36, fontWeight: "bold", textAlign: "center", marginTop: -5 }}>
+          Recetas Recomendadas 
+        </Text>
+        
+
+        <Text style={[gs.bodyText, { color: "#1565C0" }]}>
+          ¡Encuentre recetas perfectas para su bebé! 
+        </Text>
+        <Text style={[gs.bodyText, { color: "#1565C0" }]}>
+        Adecuadas en función de su edad y preferencias 
         </Text>
 
-        <View style={{ width: "90%" }}>
-          <TextInput
-            style={gs.input}
-            placeholder="Busca recetas..."
-            value={searchQuery}
-            onChangeText={handleSearch}
-          />
+        <View style={{ width: "100%" }}>
+        <View style={{ flexDirection: "row", alignItems: "center", borderWidth: 1, borderColor: "#1565C0", borderRadius: 8, opacity: 0.8, paddingHorizontal: 10 }}>
+            <TextInput
+              style={{ flex: 1, padding: 12 }}
+              placeholder="Busca recetas..."
+              value={searchQuery}
+              onChangeText={setSearchQuery}
+            />
+              <Image source={require("../../../static/images/MagnifyingGlass.png")} style={{ width: 40, height: 40, opacity: searchQuery.trim() === "" ? 1 : 1 }} />
+          </View>
 
-          <View style={{ flexDirection: "row", gap: 10, marginVertical: 10, alignSelf: "flex-start" }}>
+          <View style={{ gap: 10, marginVertical: 10, alignSelf: "flex-start",alignItems: "center",width:"100%" }}>
 
             <Link href="/recipes/favorites" style={gs.mainButton}>
               <Text style={gs.mainButtonText}>Recetas favoritas</Text>
@@ -182,22 +198,6 @@ export default function Page() {
           </View>
         </View>
 
-        {/* <Text style={[gs.subHeaderText, { marginTop: 30 }]}>Suggested recipes</Text>
-
-        {loading ? (
-          <Text>Loading recipes...</Text>
-        ) : filteredSuggestedRecipes.length === 0 ? (
-          <Text>No suggested recipes found.</Text>
-        ) : (
-          filteredSuggestedRecipes.map((recipe, index) => (
-            <Link key={index} href={`/recipes/recipeDetails?recipeTitle=${recipe.name}`}>
-              <View style={[gs.card, { marginBottom: 10 }]}>
-                <Text style={gs.cardTitle}>{recipe.name}</Text>
-                <Text style={gs.cardContent}>{recipe.description}</Text>
-              </View>
-            </Link>
-          ))
-        )} */}
 
         <Text style={[gs.subHeaderText, { marginTop: 50 }]}>Recetas recomendadas</Text>
 
@@ -263,6 +263,6 @@ export default function Page() {
           ))
         )}
       </ScrollView>
-    </View>
+    </ImageBackground>
   );
 }
