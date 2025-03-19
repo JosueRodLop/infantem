975ad3aa25f8849459761a14d1604fc109d7b841
dedece65 <danicastillo2003@gmail.com>
2025-03-19 19:38:04 +0100

    feat: edit recipes

--- frontend/app/(tabs)/recipes/index.tsx
+++ frontend/app/(tabs)/recipes/index.tsx
@@ -2,8 +2,6 @@ import { useState, useEffect } from "react";
 import { Text, View, TextInput, ScrollView, Image } from "react-native";
 import { Link } from "expo-router";
 import { Recipe } from "../../../types/Recipe";
-// import { getToken } from "../../../utils/jwtStorage"
-// import { jwtDecode } from "jwt-decode";
 import { useAuth } from "../../../context/AuthContext";
 
 
