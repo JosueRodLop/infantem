2aea7ff4aecff637cce6beeafd428d4fa9bf29ab
dedece65 <danicastillo2003@gmail.com>
2025-03-20 13:48:25 +0100

    feat: show/hide forms when not adding or editing a baby

--- frontend/app/(tabs)/baby/index.tsx
+++ frontend/app/(tabs)/baby/index.tsx
@@ -23,6 +23,8 @@ export default function BabyInfo() {
   const [selectedBaby, setSelectedBaby] = useState<Baby | null>(null);
   const [birthDateError, setBirthDateError] = useState<string | null>(null);
 
+  const [isEditing, setIsEditing] = useState(false);
+
   const isValidDate = (dateString: string) => {
     const dateRegex = /^\d{4}-\d{2}-\d{2}$/; // Formato YYYY-MM-DD
     if (!dateRegex.test(dateString)) return false;
@@ -94,6 +96,7 @@ export default function BabyInfo() {
         selectedBaby?.id ? prev.map((b) => (b.id === updatedBaby.id ? updatedBaby : b)) : [...prev, updatedBaby]
       );
       setSelectedBaby(null);
+      setIsEditing(false);
     } catch (error) {
       console.error(error);
     }
@@ -127,6 +130,15 @@ export default function BabyInfo() {
     }
   };
 
+  const handleEditBaby = (baby: Baby) => {
+    setSelectedBaby(baby);
+    setIsEditing(true);
+  }
+
+  const handleAddBaby = () => {
+    setIsEditing(true);
+  }
+
   return (
     <ImageBackground
       source={require("../../../static/images/Background.png")}
@@ -144,68 +156,70 @@ export default function BabyInfo() {
         </Text>
 
         {/* FORMULARIO */}
-        <View style={[gs.card, { padding: 20 }]}>
-          <Text style={gs.cardTitle}>{selectedBaby?.id ? "Editar bebé" : "Añadir bebé"}</Text>
-          <TextInput
-            style={gs.input}
-            placeholder="Nombre"
-            value={selectedBaby?.name || ""}
-            onChangeText={(text) => setSelectedBaby({ ...selectedBaby!, name: text })}
-          />
-          <TextInput
-            style={gs.input}
-            placeholder="Fecha de nacimiento (AAAA-MM-DD)"
-            value={selectedBaby?.birthDate || ""}
-            onChangeText={handleBirthDateChange}
-          />
-          {birthDateError && <Text style={{ color: "red" }}>{birthDateError}</Text>}
+        {isEditing && (
+          <View style={[gs.card, { padding: 20 }]}>
+            <Text style={gs.cardTitle}>{selectedBaby?.id ? "Editar bebé" : "Añadir bebé"}</Text>
+            <TextInput
+              style={gs.input}
+              placeholder="Nombre"
+              value={selectedBaby?.name || ""}
+              onChangeText={(text) => setSelectedBaby({ ...selectedBaby!, name: text })}
+            />
+            <TextInput
+              style={gs.input}
+              placeholder="Fecha de nacimiento (AAAA-MM-DD)"
+              value={selectedBaby?.birthDate || ""}
+              onChangeText={handleBirthDateChange}
+            />
+            {birthDateError && <Text style={{ color: "red" }}>{birthDateError}</Text>}
 
-          {/* Campo para el género */}
-          <Picker
-            selectedValue={selectedBaby?.genre || "OTHER"}
-            style={gs.input}
-            onValueChange={(itemValue) => setSelectedBaby({ ...selectedBaby!, genre: itemValue })}
-          >
-            <Picker.Item label="Niño" value="MALE" />
-            <Picker.Item label="Niña" value="FEMALE" />
-            <Picker.Item label="Otro" value="OTHER" />
-          </Picker>
+            {/* Campo para el género */}
+            <Picker
+              selectedValue={selectedBaby?.genre || "OTHER"}
+              style={gs.input}
+              onValueChange={(itemValue) => setSelectedBaby({ ...selectedBaby!, genre: itemValue })}
+            >
+              <Picker.Item label="Niño" value="MALE" />
+              <Picker.Item label="Niña" value="FEMALE" />
+              <Picker.Item label="Otro" value="OTHER" />
+            </Picker>
 
-          <TextInput
-            style={gs.input}
-            placeholder="Peso (kg)"
-            keyboardType="decimal-pad"
-            value={selectedBaby?.weight?.toString() || ""}
-            onChangeText={(text) => handleDecimalInput(text, "weight")}
-          />
+            <TextInput
+              style={gs.input}
+              placeholder="Peso (kg)"
+              keyboardType="decimal-pad"
+              value={selectedBaby?.weight?.toString() || ""}
+              onChangeText={(text) => handleDecimalInput(text, "weight")}
+            />
 
-          <TextInput
-            style={gs.input}
-            placeholder="Altura (cm)"
-            keyboardType="decimal-pad"
-            value={selectedBaby?.height?.toString() || ""}
-            onChangeText={(text) => handleDecimalInput(text, "height")}
-          />
+            <TextInput
+              style={gs.input}
+              placeholder="Altura (cm)"
+              keyboardType="decimal-pad"
+              value={selectedBaby?.height?.toString() || ""}
+              onChangeText={(text) => handleDecimalInput(text, "height")}
+            />
 
-          <TextInput
-            style={gs.input}
-            placeholder="Perímetro cefálico (cm)"
-            keyboardType="decimal-pad"
-            value={selectedBaby?.cephalicPerimeter?.toString() || ""}
-            onChangeText={(text) => handleDecimalInput(text, "cephalicPerimeter")}
-          />
+            <TextInput
+              style={gs.input}
+              placeholder="Perímetro cefálico (cm)"
+              keyboardType="decimal-pad"
+              value={selectedBaby?.cephalicPerimeter?.toString() || ""}
+              onChangeText={(text) => handleDecimalInput(text, "cephalicPerimeter")}
+            />
 
-          <TextInput
-            style={gs.input}
-            placeholder="Preferencias alimentarias"
-            value={selectedBaby?.foodPreference || ""}
-            onChangeText={(text) => setSelectedBaby({ ...selectedBaby!, foodPreference: text })}
-          />
+            <TextInput
+              style={gs.input}
+              placeholder="Preferencias alimentarias"
+              value={selectedBaby?.foodPreference || ""}
+              onChangeText={(text) => setSelectedBaby({ ...selectedBaby!, foodPreference: text })}
+            />
 
-          <TouchableOpacity style={gs.mainButton} onPress={handleSaveBaby}>
-            <Text style={gs.mainButtonText}>{selectedBaby?.id ? "Actualizar" : "Guardar"}</Text>
-          </TouchableOpacity>
-        </View>
+            <TouchableOpacity style={gs.mainButton} onPress={handleSaveBaby}>
+              <Text style={gs.mainButtonText}>{selectedBaby?.id ? "Actualizar" : "Guardar"}</Text>
+            </TouchableOpacity>
+          </View>
+        )}
 
         {/* LISTADO DE BEBÉS */}
         <Text style={[gs.subHeaderText, { color: "#1565C0", marginBottom: 10, fontWeight: "bold" }]}>Mis bebés registrados</Text>
@@ -231,7 +245,7 @@ export default function BabyInfo() {
                 <Text style={gs.cardContent}>📏 Altura: {baby.height} cm</Text>
               </View>
 
-              <TouchableOpacity style={gs.mainButton} onPress={() => setSelectedBaby(baby)}>
+              <TouchableOpacity style={gs.mainButton} onPress={() => handleEditBaby(baby)}>
                 <Text style={gs.mainButtonText}>Editar</Text>
               </TouchableOpacity>
               <TouchableOpacity style={[gs.mainButton, { backgroundColor: "red" }]} onPress={() => handleDeleteBaby(baby.id!)}>
@@ -239,6 +253,11 @@ export default function BabyInfo() {
               </TouchableOpacity>
             </View>
           )))}
+        {!isEditing && (
+          <TouchableOpacity style={[gs.mainButton, { backgroundColor: "#1565C0" }]} onPress={handleAddBaby}>
+            <Text style={gs.mainButtonText}>{"Añadir un bebé"}</Text>
+          </TouchableOpacity>
+        )}
       </ScrollView>
     </ImageBackground>
   );
