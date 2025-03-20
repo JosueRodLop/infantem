7c2417ade62ae0ff09993825d033cf39ca450693
Luis-Giraldo-Santiago <luisgirsan@gmail.com>
2025-03-20 17:47:28 +0100

    fix: change controller to work with Stripe. Refs #179

--- src/main/java/com/isppG8/infantem/infantem/subscription/SubscriptionController.java
+++ src/main/java/com/isppG8/infantem/infantem/subscription/SubscriptionController.java
@@ -2,6 +2,8 @@ package com.isppG8.infantem.infantem.subscription;
 
 import com.isppG8.infantem.infantem.payment.Payment;
 import com.isppG8.infantem.infantem.payment.PaymentService;
+import com.isppG8.infantem.infantem.user.User;
+import com.isppG8.infantem.infantem.user.UserService;
 import com.stripe.exception.StripeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,35 +15,23 @@ import java.util.Optional;
 @RequestMapping("api/v1/subscriptions")
 public class SubscriptionController {
 
-    @Autowired
-    private SubscriptionService subscriptionService;
+    private final SubscriptionService subscriptionService;
+    private final UserService userService;
 
-    @Autowired
-    private PaymentService paymentService;
+    public SubscriptionController(SubscriptionService subscriptionService, UserService userService) {
+        this.subscriptionService = subscriptionService;
+        this.userService = userService;
+    }
 
     @PostMapping("/start/{userId}/{priceId}")
     public ResponseEntity<String> startSubscription(@PathVariable Long userId, @PathVariable String priceId) {
-        Optional<Payment> payment = paymentService.getPaymentByUserId(userId);
-
-        if (payment.isEmpty()) {
-            return ResponseEntity.badRequest().body("Usuario no tiene método de pago en Stripe");
-        }
+        User user = userService.getUserById(userId);
 
         try {
-            String subscriptionId = subscriptionService.createSubscription(payment.get().getStripeCustomerId(), priceId);
+            String subscriptionId = subscriptionService.createSubscription(user, priceId);
             return ResponseEntity.ok("Suscripción creada con éxito. ID: " + subscriptionId);
-        } catch (StripeException e) {
+        } catch (StripeException | IllegalStateException e) {
             return ResponseEntity.badRequest().body("Error al crear suscripción: " + e.getMessage());
-        }
-    }
-
-    @PostMapping("/cancel/{subscriptionId}")
-    public ResponseEntity<String> cancelSubscription(@PathVariable String subscriptionId) {
-        try {
-            subscriptionService.cancelSubscription(subscriptionId);
-            return ResponseEntity.ok("Suscripción cancelada con éxito");
-        } catch (StripeException e) {
-            return ResponseEntity.badRequest().body("Error al cancelar suscripción: " + e.getMessage());
         }
     }
 }
