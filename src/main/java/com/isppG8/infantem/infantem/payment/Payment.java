5206f73431740360f1d39fe5572fbdc530f44ae8
Luis-Giraldo-Santiago <luisgirsan@gmail.com>
2025-03-20 16:23:22 +0100

    fix: adapted class to work with Stripe. Refs #179

--- src/main/java/com/isppG8/infantem/infantem/payment/Payment.java
+++ src/main/java/com/isppG8/infantem/infantem/payment/Payment.java
@@ -1,23 +1,10 @@
 package com.isppG8.infantem.infantem.payment;
 
-import com.isppG8.infantem.infantem.user.User;
-
-import io.micrometer.common.lang.NonNull;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
-import org.jasypt.util.text.StrongTextEncryptor;
-import java.sql.Types;
-import org.hibernate.annotations.JdbcTypeCode;
+import com.isppG8.infantem.infantem.user.User;
+import org.springframework.lang.NonNull;
 
 @Entity
 @Table(name = "payment_table")
@@ -30,33 +17,15 @@ public class Payment {
 
     @NonNull
     @Enumerated(EnumType.STRING)
-    private PaymentType paymentType;  // PAYPAL, CARD
+    private PaymentType paymentType;  // CARD, OTHER
 
-    @JdbcTypeCode(Types.VARCHAR)
-    private String paypalEmail;
-
-    @JdbcTypeCode(Types.VARCHAR)
-    private String billingAgreementId;
+    @NonNull
+    private String stripeCustomerId;  // ID del cliente en Stripe
 
     private Boolean isDefault;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
-
-    // Método para cifrar datos antes de guardarlos
-    public void encryptData(String secretKey) {
-        StrongTextEncryptor encryptor = new StrongTextEncryptor();
-        encryptor.setPassword(secretKey);
-        this.paypalEmail = encryptor.encrypt(this.paypalEmail);
-        this.billingAgreementId = encryptor.encrypt(this.billingAgreementId);
-    }
-
-    // Método para descifrar datos cuando se leen
-    public void decryptData(String secretKey) {
-        StrongTextEncryptor encryptor = new StrongTextEncryptor();
-        encryptor.setPassword(secretKey);
-        this.paypalEmail = encryptor.decrypt(this.paypalEmail);
-        this.billingAgreementId = encryptor.decrypt(this.billingAgreementId);
-    }
 }
+
