08890b1e230125a753df49dac42285d3bf64a3b8
Luis-Giraldo-Santiago <luisgirsan@gmail.com>
2025-03-20 16:23:30 +0100

    fix: adapted class to work with Stripe. Refs #179

--- src/main/java/com/isppG8/infantem/infantem/subscription/Subscription.java
+++ src/main/java/com/isppG8/infantem/infantem/subscription/Subscription.java
@@ -1,25 +1,14 @@
 package com.isppG8.infantem.infantem.subscription;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-
-import com.isppG8.infantem.infantem.payment.Payment;
-import com.isppG8.infantem.infantem.user.User;
-
-import io.micrometer.common.lang.NonNull;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.Positive;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import com.isppG8.infantem.infantem.user.User;
+import org.springframework.lang.NonNull;
+import java.time.LocalDate;
 
 @Entity
-@Table(name = "subcription_table")
+@Table(name = "subscription_table")
 @Getter @Setter
 public class Subscription {
 
@@ -28,8 +17,7 @@ public class Subscription {
     private Long id;
 
     @NonNull
-    @Positive
-    private Double price;
+    private String stripeSubscriptionId;
 
     @NonNull
     private LocalDate startDate;
@@ -40,18 +28,4 @@ public class Subscription {
     @OneToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
-
-    @OneToOne
-    @JoinColumn(name = "payment_id")
-    private Payment payment;
-
-
-    public LocalDate getNextBillingDate() {
-        LocalDate today = LocalDate.now();
-        long periodsPassed = ChronoUnit.DAYS.between(startDate, today) / 28;
-        return startDate.plusDays((periodsPassed + 1) * 28);
-    }
-
-
-    
 }
