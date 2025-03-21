69d468d394ee11c5d8a4c4a91c22da94903b5344
Luis-Giraldo-Santiago <luisgirsan@gmail.com>
2025-03-21 11:13:23 +0100

    fix: add route for new query. Refs #179

--- src/main/java/com/isppG8/infantem/infantem/user/UserService.java
+++ src/main/java/com/isppG8/infantem/infantem/user/UserService.java
@@ -11,12 +11,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.isppG8.infantem.infantem.exceptions.ResourceNotFoundException;
+import com.isppG8.infantem.infantem.subscription.SubscriptionInfantemRepository;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private SubscriptionInfantemRepository subscriptionInfantemRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -103,7 +107,7 @@ public class UserService {
     }
 
     public Optional<User> getUserByStripeCustomerId(String stripeCustomerId) {
-        return userRepository.findByStripeCustomerId(stripeCustomerId);
+        return subscriptionInfantemRepository.findByStripeCustomerId(stripeCustomerId);
     }
 
 
