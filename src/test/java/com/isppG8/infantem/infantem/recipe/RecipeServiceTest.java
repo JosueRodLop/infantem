737397d0f4b62b76adbaf802a08a12357dfd776b
Miguel Galán Lerate <miggaller@alum.us.es>
2025-03-21 17:49:04 +0100

    feat: add max age recipe filter test

--- src/test/java/com/isppG8/infantem/infantem/recipe/RecipeServiceTest.java
+++ src/test/java/com/isppG8/infantem/infantem/recipe/RecipeServiceTest.java
@@ -21,12 +21,12 @@ public class RecipeServiceTest {
         this.recipeService = recipeService;
     }
 
-    final int BABY_AGE = 6;
     final int HUGE_BABY_AGE = 999;
 
     @Test
     public void recipeFilterByMinAgeTest() {
         final List<Recipe> allRecipes = recipeService.findAllRecipes();
+        final int BABY_AGE = 6;
 
         List<Recipe> filtered_recipes = recipeService.getRecipeByMinAge(BABY_AGE);
         assertTrue(filtered_recipes.size() < allRecipes.size(),
@@ -41,6 +41,28 @@ public class RecipeServiceTest {
         assertTrue(emptyFilteredRecipe.isEmpty(), "This filtered list should be empty: " + emptyFilteredRecipe);
 
         List<Recipe> noFilteredRecipes = recipeService.getRecipeByMinAge(HUGE_BABY_AGE);
+        assertEquals(allRecipes, noFilteredRecipes, "All recipes should have min age less than" + HUGE_BABY_AGE);
+    }
+
+    @Test
+    public void recipeFilterByMaxAgeTest() {
+        final List<Recipe> allRecipes = recipeService.findAllRecipes();
+        final int BABY_AGE = 10;
+
+        List<Recipe> filtered_recipes = recipeService.getRecipeByMaxAge(BABY_AGE);
+        assertTrue(filtered_recipes.size() <= allRecipes.size(),
+                "Filtered recipe list size should be less than original list, Filtered size: " + filtered_recipes.size()
+                        + "Original size: " + allRecipes.size());
+
+        Boolean allRecipesHasCorrectMinAge = filtered_recipes.stream()
+                .allMatch(recipe -> BABY_AGE <= recipe.getMaxRecommendedAge());
+
+        assertTrue(allRecipesHasCorrectMinAge, "All recipes should has min recommended age greater than " + BABY_AGE);
+
+        List<Recipe> emptyFilteredRecipe = recipeService.getRecipeByMaxAge(HUGE_BABY_AGE);
+        assertTrue(emptyFilteredRecipe.isEmpty(), "This filtered list should be empty: " + emptyFilteredRecipe);
+
+        List<Recipe> noFilteredRecipes = recipeService.getRecipeByMaxAge(0);
         assertEquals(allRecipes, noFilteredRecipes, "All recipes should have min age greater than 0");
     }
 
