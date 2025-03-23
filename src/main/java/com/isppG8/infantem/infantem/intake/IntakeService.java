a3b77c746bca6d2ddec78734205666e2bf6ae694
DavidFuentelsazRodriguez <davfuerod@alum.us.es>
2025-03-23 09:36:52 +0100

    feat: enhance intake management with DTOs and validation

--- src/main/java/com/isppG8/infantem/infantem/intake/IntakeService.java
+++ src/main/java/com/isppG8/infantem/infantem/intake/IntakeService.java
@@ -1,30 +1,77 @@
 package com.isppG8.infantem.infantem.intake;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.isppG8.infantem.infantem.baby.Baby;
+import com.isppG8.infantem.infantem.baby.BabyService;
+import com.isppG8.infantem.infantem.exceptions.ResourceNotFoundException;
+import com.isppG8.infantem.infantem.exceptions.ResourceNotOwnedException;
+import com.isppG8.infantem.infantem.user.User;
+import com.isppG8.infantem.infantem.user.UserService;
 
 @Service
 public class IntakeService {
 
-    @Autowired
-    private IntakeRepository intakeRepository;
+    private final IntakeRepository intakeRepository;
+    private final UserService userService;
+    private final BabyService babyService;
 
+    @Autowired
+    public IntakeService(IntakeRepository intakeRepository, UserService userService, BabyService babyService) {
+        this.intakeRepository = intakeRepository;
+        this.userService = userService;
+        this.babyService = babyService;
+    }
+
+    @Transactional(readOnly = true)
     public List<Intake> getAllIntakes() {
         return intakeRepository.findAll();
     }
 
-    public Optional<Intake> getIntakeById(Long id) {
-        return intakeRepository.findById(id);
+    @Transactional(readOnly = true)
+    public Intake getIntakeById(Long id) {
+        checkOwnerShip(this.intakeRepository.findById(id).get());
+        return this.intakeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Intake", "id", id));
     }
 
+    @Transactional
     public Intake saveIntake(Intake intake) {
-        return intakeRepository.save(intake);
+        checkOwnerShip(intake);
+        return this.intakeRepository.save(intake);
     }
 
+    @Transactional
+    public Intake updateIntake(Long id, Intake intake) {
+        checkOwnerShip(intake);
+        return this.intakeRepository.findById(id).map(existingIntake -> {
+            existingIntake.setDate(intake.getDate());
+            existingIntake.setQuantity(intake.getQuantity());
+            existingIntake.setObservations(intake.getObservations());
+            existingIntake.setRecipes(intake.getRecipes());
+            existingIntake.setIntakeSymptom(intake.getIntakeSymptom());
+            return this.intakeRepository.save(existingIntake);
+        }).orElseThrow(() -> new ResourceNotFoundException("Intake", "id", intake.getId()));
+    }
+
+    @Transactional
     public void deleteIntake(Long id) {
-        intakeRepository.deleteById(id);
+        checkOwnerShip(this.intakeRepository.findById(id).get());
+        if (!this.intakeRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Intake", "id", id);
+        }
+        this.intakeRepository.deleteById(id);
+    }
+
+    private void checkOwnerShip(Intake intake) {
+        User user = this.userService.findCurrentUser();
+        Baby baby = this.babyService.findById(intake.getBaby().getId());
+        if (!baby.getUsers().contains(user)) {
+            throw new ResourceNotOwnedException(intake.getBaby());
+        }
+
     }
 }
