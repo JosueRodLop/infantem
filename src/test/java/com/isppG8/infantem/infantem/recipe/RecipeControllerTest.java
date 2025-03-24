3999756dab5d5a78bfc549f7c84271c987b8390a
Miguel Galán Lerate <miggaller@alum.us.es>
2025-03-24 14:55:05 +0100

    feat: Add controller tests

---
+++ src/test/java/com/isppG8/infantem/infantem/recipe/RecipeControllerTest.java
@@ -0,0 +1,58 @@
+package com.isppG8.infantem.infantem.recipe;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import com.isppG8.infantem.infantem.user.User;
+import com.isppG8.infantem.infantem.user.UserService;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+@AutoConfigureMockMvc
+public class RecipeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Autowired
+    private RecipeController recipeController;
+
+    @BeforeEach
+    void setUp() {
+        User user = new User();
+        user.setId(1);
+        Mockito.when(userService.findCurrentUser()).thenReturn(user);
+        mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+    }
+
+    @Test
+    void testGetAllRecipes() throws Exception {
+
+        mockMvc.perform(get("/api/v1/recipes").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content.length()").value(5))
+                .andExpect(jsonPath("$.content[0].name").value("Puré de Zanahoria y Batata"));
+    }
+
+    @Test
+    void testGetRecipeById() throws Exception {
+
+        mockMvc.perform(get("/api/v1/recipes/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Puré de Zanahoria y Batata"));
+    }
+}
