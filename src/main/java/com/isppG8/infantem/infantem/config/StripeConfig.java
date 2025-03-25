f6f7a4daca322d707ce899fc9179e81a94159ba0
Luis-Giraldo-Santiago <luisgirsan@gmail.com>
2025-03-25 17:53:11 +0100

    fix: change the secret into .env. Refs #179

--- src/main/java/com/isppG8/infantem/infantem/config/StripeConfig.java
+++ src/main/java/com/isppG8/infantem/infantem/config/StripeConfig.java
@@ -1,17 +1,26 @@
 package com.isppG8.infantem.infantem.config;
 
 import com.stripe.Stripe;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StripeConfig {
 
-    @Value("${stripe.secret.key}")
-    private String stripeSecretKey;
+    private final String stripeApiKey;
 
-    @jakarta.annotation.PostConstruct
-    public void init() {
-        Stripe.apiKey = stripeSecretKey;
+    public StripeConfig() {
+        Dotenv dotenv = Dotenv.load();
+        this.stripeApiKey = dotenv.get("STRIPE_SECRET_KEY");
+
+        // Configurar la clave de API de Stripe globalmente
+        Stripe.apiKey = this.stripeApiKey;
+    }
+
+    public String getStripeApiKey() {
+        return stripeApiKey;
     }
 }
