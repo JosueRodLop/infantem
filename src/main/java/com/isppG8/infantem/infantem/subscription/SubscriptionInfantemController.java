36b7066b8e2ac739622014e89583b9d7ec419be1
Luis-Giraldo-Santiago <luisgirsan@gmail.com>
2025-03-25 17:53:40 +0100

    fix: clean controller. Refs #179

--- src/main/java/com/isppG8/infantem/infantem/subscription/SubscriptionInfantemController.java
+++ src/main/java/com/isppG8/infantem/infantem/subscription/SubscriptionInfantemController.java
@@ -1,13 +1,9 @@
 package com.isppG8.infantem.infantem.subscription;
 
-import com.isppG8.infantem.infantem.user.User;
-import com.isppG8.infantem.infantem.user.UserService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,9 +15,6 @@ public class SubscriptionInfantemController {
 
     @Autowired
     private SubscriptionInfantemService subscriptionService;
-
-    @Autowired
-    private UserService userService;
 
     // Crear una suscripción
     @PostMapping("/create")
