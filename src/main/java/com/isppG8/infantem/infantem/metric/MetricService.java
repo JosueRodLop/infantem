86932edc2d40a67d256a7949a9317322b6517047
Lucía Noya Cano <lucnoycan@alum.us.es>
2025-03-25 23:10:22 +0100

    feat:complete CRUD Metric entity Refs #249

--- src/main/java/com/isppG8/infantem/infantem/metric/MetricService.java
+++ src/main/java/com/isppG8/infantem/infantem/metric/MetricService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 
 import com.isppG8.infantem.infantem.metric.dto.MetricSummary;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class MetricService {
 
@@ -18,7 +20,35 @@ public class MetricService {
         this.metricRepository = metricRepository;
     }
 
-    // TODO: finish service
+    public Metric createMetric(Metric metric) {
+        return metricRepository.save(metric);
+    }
+
+    public Metric getMetricById(Long id) {
+        return metricRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Metric not found with id: " + id));
+    }
+
+    public List<Metric> getAllMetricsByBabyId(Integer babyId) {
+        return metricRepository.findAll().stream()
+                .filter(metric -> metric.getBaby().getId().equals(babyId))
+                .toList();
+    }
+
+    public Metric updateMetric(Long id, Metric updatedMetric) {
+        Metric metric = getMetricById(id);
+        metric.setWeight(updatedMetric.getWeight());
+        metric.setHeight(updatedMetric.getHeight());
+        metric.setCephalicPerimeter(updatedMetric.getCephalicPerimeter());
+        metric.setDate(updatedMetric.getDate());
+        return metricRepository.save(metric);
+    }
+
+    public void deleteMetric(Long id) {
+        Metric metric = getMetricById(id);
+        metricRepository.delete(metric);
+    }
+
 
     // Methods for calendar
 
