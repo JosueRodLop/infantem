0dbd53e2502675dc2e005333dabf5200d2ea0566
Luis-Giraldo-Santiago <luisgirsan@gmail.com>
2025-03-25 18:01:46 +0100

    fix: change relation. Refs #179

--- src/main/java/com/isppG8/infantem/infantem/subscription/SubscriptionInfantem.java
+++ src/main/java/com/isppG8/infantem/infantem/subscription/SubscriptionInfantem.java
@@ -17,7 +17,7 @@ public class SubscriptionInfantem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
