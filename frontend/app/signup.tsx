eab88f6e9ca48e314b10fab656cb7e7f9c8f69b4
Antonio Jiménez Ortega <antjimort@alum.us.es>
2025-03-25 19:45:21 +0100

    fix: handle signup errors for every input Ref #256

--- frontend/app/signup.tsx
+++ frontend/app/signup.tsx
@@ -22,54 +22,81 @@ export default function Signup() {
   const apiUrl = process.env.EXPO_PUBLIC_API_URL;
   const gs = require("../static/styles/globalStyles");
 
-  const handleSubmit = async () => {
-    try {
-      const signupResponse = await fetch(`${apiUrl}/api/v1/auth/signup`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: name,
-          surname: surname,
-          username: username,
-          email: email,
-          password: password,
-        }),
-      });
-
-      if (!signupResponse.ok) {
-        setErrorMessage("Algo no ha ido bien.");
-        return;
-      } else if (!acceptedTerms) {
-        setErrorMessage("Debes leer y aceptar los términos y condiciones.");
-        return;
-      }
-
-      // Autologin después del registro
-      const signinResponse = await fetch(`${apiUrl}/api/v1/auth/signin`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: username, 
-          password: password,
-        }),
-      });
-
-      if (!signinResponse.ok) {
-        setErrorMessage("Algo no ha ido bien");
-        return;
-      }
-
-      const data = await signinResponse.json();
-      await storeToken(data.token);
-      router.push("/recipes");
-
-    } catch (error) {
-      console.error("An error ocurred: ", error);
+  const validEmail = (email) => {
+    if (email.match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )) {
+      return true;
     }
+    return false;
+  };
+
+  const handleSubmit = async () => {
+    if (!name || !surname || !username || !email || !password) {
+      setErrorMessage("Debes rellenar todos los campos.");
+      return;
+    } else if (!validEmail(email)) {
+      setErrorMessage("El email no es válido.");
+      return;
+    } else if (name.length < 3) {
+      setErrorMessage("El nombre debe tener al menos 3 caracteres.");
+      return;
+    } else if (surname.length < 3) {
+      setErrorMessage("El apellido debe tener al menos 3 caracteres.");
+      return;
+    } else if (username.length < 3) {
+      setErrorMessage("Tu nombre de usuario debe tener al menos 3 caracteres.");
+      return;
+    } else {
+      try {
+        const signupResponse = await fetch(`${apiUrl}/api/v1/auth/signup`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: name,
+            surname: surname,
+            username: username,
+            email: email,
+            password: password,
+          }),
+        });
+
+        if (!signupResponse.ok) {
+          setErrorMessage("Algo no ha ido bien.");
+          return;
+        } else if (!acceptedTerms) {
+          setErrorMessage("Debes leer y aceptar los términos y condiciones.");
+          return;
+        }
+
+        // Autologin después del registro
+        const signinResponse = await fetch(`${apiUrl}/api/v1/auth/signin`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: username,
+            password: password,
+          }),
+        });
+
+        if (!signinResponse.ok) {
+          setErrorMessage("Algo no ha ido bien");
+          return;
+        }
+
+        const data = await signinResponse.json();
+        await storeToken(data.token);
+        router.push("/recipes");
+
+      } catch (error) {
+        console.error("An error ocurred: ", error);
+      }
+    }
+
   };
 
   const handleAcceptTerms = () => {
@@ -86,37 +113,37 @@ export default function Signup() {
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
       <View style={[gs.container, { justifyContent: "center", alignItems: "center", backgroundColor: "#E3F2FD", flex: 1, paddingVertical: 40 }]}>
-        
+
         <Image source={require("../static/images/profile.webp")} style={{ width: 100, height: 100, borderRadius: 50, marginBottom: 20 }} />
 
         <View style={[gs.card, { maxWidth: 400, width: "90%", padding: 25, borderRadius: 15, backgroundColor: "white", shadowColor: "#000", shadowOpacity: 0.1, shadowRadius: 10, elevation: 5 }]}>
-          
+
           <Text style={{ fontSize: 24, fontWeight: "bold", color: "#1565C0", textAlign: "center", marginBottom: 15 }}>
             Regístrate
           </Text>
 
-          <TextInput 
-            style={[gs.input, { padding: 12, borderRadius: 8, borderWidth: 1, borderColor: "#1565C0" , opacity:0.6}]}
+          <TextInput
+            style={[gs.input, { padding: 12, borderRadius: 8, borderWidth: 1, borderColor: "#1565C0", opacity: 0.6 }]}
             placeholder="Nombre" value={name} onChangeText={setName} />
-          <TextInput 
-            style={[gs.input, { padding: 12, borderRadius: 8, borderWidth: 1, borderColor: "#1565C0" , opacity:0.6}]}
+          <TextInput
+            style={[gs.input, { padding: 12, borderRadius: 8, borderWidth: 1, borderColor: "#1565C0", opacity: 0.6 }]}
             placeholder="Apellido" value={surname} onChangeText={setSurname} />
-          <TextInput 
-            style={[gs.input, { padding: 12, borderRadius: 8, borderWidth: 1, borderColor: "#1565C0" , opacity:0.6}]}
+          <TextInput
+            style={[gs.input, { padding: 12, borderRadius: 8, borderWidth: 1, borderColor: "#1565C0", opacity: 0.6 }]}
             placeholder="Nombre de usuario" value={username} onChangeText={setUsername} autoCapitalize="none" />
-          <TextInput 
-            style={[gs.input, { padding: 12, borderRadius: 8, borderWidth: 1, borderColor: "#1565C0" , opacity:0.6}]}
+          <TextInput
+            style={[gs.input, { padding: 12, borderRadius: 8, borderWidth: 1, borderColor: "#1565C0", opacity: 0.6 }]}
             placeholder="Email" value={email} onChangeText={setEmail} autoCapitalize="none" keyboardType="email-address" />
 
           <View style={{ position: "relative" }}>
             <TextInput
-            style={[gs.input, { padding: 12, borderRadius: 8, borderWidth: 1, borderColor: "#1565C0" , opacity:0.6}]}
-            placeholder="Contraseña"
+              style={[gs.input, { padding: 12, borderRadius: 8, borderWidth: 1, borderColor: "#1565C0", opacity: 0.6 }]}
+              placeholder="Contraseña"
               value={password}
               onChangeText={setPassword}
               secureTextEntry={!showPassword} // Alternar visibilidad
             />
-            <TouchableOpacity 
+            <TouchableOpacity
               onPress={() => setShowPassword(!showPassword)}
               style={{ position: "absolute", right: 50, top: 20 }} // Icono alineado
             >
@@ -130,7 +157,7 @@ export default function Signup() {
             <CheckBox
               style={[{ padding: 10 }]}
               onClick={() => {
-                setAcceptedTerms(!acceptedTerms)
+                setAcceptedTerms(!acceptedTerms);
               }}
               isChecked={acceptedTerms}
               disabled={!openedTerms}
@@ -140,8 +167,8 @@ export default function Signup() {
               <Text
                 style={{ color: "#007AFF", fontSize: 14 }}
                 onPress={() => {
-                  setModalVisible(true)
-                  setOpenedTerms(true)
+                  setModalVisible(true);
+                  setOpenedTerms(true);
                 }}
               >
                 términos y condiciones
@@ -157,7 +184,7 @@ export default function Signup() {
             <Text style={{ color: "#007AFF", fontSize: 14 }}>¿Ya tienes cuenta? ¡Inicia sesión!</Text>
           </Link>
 
-          <TouchableOpacity 
+          <TouchableOpacity
             style={{ marginTop: 20, backgroundColor: "#1565C0", padding: 14, borderRadius: 8, alignItems: "center", shadowColor: "#000", shadowOpacity: 0.2, shadowRadius: 5, elevation: 3 }}
             onPress={handleSubmit}
           >
