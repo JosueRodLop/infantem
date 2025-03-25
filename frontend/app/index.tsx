7e6255b0afe7c73cbb445b86ddf5f5c8054eabf2
angelalopol03 <Angela Lpez>
2025-03-25 19:58:25 +0100

    feat: Add new images and fonts, enhance layout with custom header

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,69 +1,193 @@
-import { Link, Redirect } from "expo-router";
-import { View, Text, ImageBackground, Image } from "react-native";
+import { Link, Redirect, SplashScreen } from "expo-router";
+import { View, Text, Image, Dimensions, ScrollView } from "react-native";
 import { useAuth } from "../context/AuthContext";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function Index() {
   const { isAuthenticated } = useAuth();
-
   const gs = require("../static/styles/globalStyles");
+  const { height } = Dimensions.get("window");
 
-  if (isAuthenticated)
-    return <Redirect href="recipes" />
+  const [fontsLoaded] = useFonts({
+    "Loubag-Regular": require("../assets/fonts/Loubag-Regular.ttf"),
+    "Loubag-Medium": require("../assets/fonts/Loubag-Medium.ttf"),
+    "Loubag-Bold": require("../assets/fonts/Loubag-Bold.ttf"),
+    "Loubag-Light": require("../assets/fonts/Loubag-Light.ttf"),
+  });
+
+  const features = [
+    {
+      title: "1. Recomendaciones",
+      desc: "Recetas personalizadas para ti y tu bebé según sus necesidades.",
+      icon: require("../static/images/Pot.png"), 
+    },
+    {
+      title: "2. Detección de Alergias",
+      desc: "Identifica ingredientes peligrosos con alertas inteligentes.",
+      icon: require("../static/images/Inhaler.png"),
+    },
+    {
+      title: "3. MarketPlace",
+      desc: "Compra productos confiables y aprobados en un solo lugar.",
+      icon: require("../static/images/Marketplace.png"),
+    },
+    {
+      title: "4. Calendario Inteligente",
+      desc: "Organiza hitos, recordatorios y seguimiento del desarrollo.",
+      icon: require("../static/images/Calendar.png"),
+    },
+  ];
+
+  const { width } = Dimensions.get("window");
+  const isMobile = width < 768;
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+  if (isAuthenticated) return <Redirect href="recipes" />;
 
   return (
-    <ImageBackground
-      source={require("../static/images/Background.png")}
-      style={{ flex: 1, width: "100%", height: "100%", justifyContent: "center" }}
-      imageStyle={{ resizeMode: "cover", opacity: 0.9 }}
-    >
-      <View style={[gs.container, { flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingHorizontal: 20, backgroundColor: "transparent", width: "100%" }]}>
+    <View style={{ flex: 1, backgroundColor: "#E3F2FD" }}>
+      <ScrollView contentContainerStyle={{ padding: 10, paddingBottom: 200 }}>
+      <View style={{ width: "110%", height: height  }}>
+        <Image
+          source={require("../static/images/Background.png")}
+          style={{
+            width: "100%",
+            height: "100%",
+            resizeMode: "cover",
+          }}
+        />
+        <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              paddingHorizontal:"10%",
+              position: "absolute",
+              top: 160,}}
+          >
+            <Text
+              style={{
+                color: "#1565C0",
+                fontFamily: "Loubag-Regular",
+                fontSize: 36,
+              }}
+            >
+              El mejor cuidado{" "}
+              
+            </Text>
+            <Text style={{
+                color: "#1565C0",
+                fontFamily: "Loubag-Bold",
+                fontSize: 50,
+                marginBottom: 30,}}>para tu bebé</Text>
 
-        <View style={{ flex: 1, padding: 20 }}>
-
-          <Text style={{ color: "#1565C0", fontSize: 60, fontWeight: "bold", textAlign: "left", width: 500 }}>
-            <Text style={{ fontWeight: "bold", fontStyle: "italic" }}>INFANTEM</Text>
-          </Text>
-          <Text style={{ color: "#1565C0", fontSize: 36, fontWeight: "bold", textAlign: "left", width: "100%" }}>
-            El mejor cuidado <Text style={{ fontStyle: "italic" }}>para tu bebé</Text>
-          </Text>
-
-          <Text style={{ color: "#1565C0", fontSize: 20, textAlign: "left", marginTop: 20 }}>
-            <Text style={{ fontStyle: "italic" }}>Recomendaciones Personalizadas</Text>
-          </Text>
-          <Text style={{ color: "#1565C0", fontSize: 20, textAlign: "left", marginTop: 5 }}>
-            <Text style={{ fontStyle: "italic" }}>Validado por Expertos</Text>
-          </Text>
-          <Text style={{ color: "#1565C0", fontSize: 20, textAlign: "left", marginTop: 5 }}>
-            <Text style={{ fontStyle: "italic" }}>Contenido actualizado</Text>
-          </Text>
-          <Text style={{ color: "#1565C0", fontSize: 20, textAlign: "left", marginTop: 5 }}>
-            <Text style={{ fontStyle: "italic" }}>Acceso rápido y sencillo</Text>
-          </Text>
-
-          <View style={{ marginTop: 20, gap: 10 }}>
-            <Link style={[gs.mainButton, { width: 150, backgroundColor: "#1565C0", padding: 12, borderRadius: 8, alignItems: "center" }]} href={"/signin"}>
-              <Text style={[gs.mainButtonText, { color: "#FFFFFF", fontSize: 16, fontWeight: "bold" }]}>
-                Inicia sesión
-              </Text>
-            </Link>
-
-            <Link style={[gs.secondaryButton, { width: 250, backgroundColor: "#E3F2FD", padding: 12, borderRadius: 8, borderWidth: 1, borderColor: "#1565C0", alignItems: "center" }]} href={"/signup"}>
-              <Text style={[gs.secondaryButtonText, { color: "#1565C0", fontSize: 16, fontWeight: "bold" }]}>
-                ¿No tienes cuenta? Únete
+            <Link
+              style={[
+                gs.secondaryButton,
+                {
+                  width: 180,
+                  backgroundColor: "#1565C0",
+                  padding: 12,
+                  borderRadius: 8,
+                  borderWidth: 1,
+                  borderColor: "#1565C0",
+                  alignItems: "center",
+                },
+              ]}
+              href={"/signup"}
+            >
+              <Text
+                style={[
+                  gs.secondaryButtonText,
+                  { color: "white", fontSize: 16, fontWeight: "bold" },
+                ]}
+              >
+                ¡Empieza ahora!
               </Text>
             </Link>
           </View>
-        </View>
-
-        <View style={{ flex: 1 }}>
-          <Image
-            source={require("../static/images/food.png")}
-            style={{ width: "115%", resizeMode: "cover", borderTopLeftRadius: 200, borderBottomLeftRadius: 200 }}
-          />
-        </View>
-
       </View>
+        
+       
+{/*---------------------------------------------------------------------------------------------------------------------------- */}
+        <View
+      style={{
+        backgroundColor: "#BBDEFB",
+        padding: 30,
+        borderRadius: 20,
+        marginHorizontal: 20,
+        marginTop: 60,
+        marginBottom: 100,
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 28,
+          fontFamily: "Loubag-Bold",
+          textAlign: "center",
+          color: "#0D47A1",
+          marginBottom: 30,
+        }}
+      >
+        ¿Cómo te ayudamos?
+      </Text>
 
-    </ImageBackground>
+      <View
+        style={{
+          flexDirection: isMobile ? "column" : "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          gap: 30,
+          flexWrap: "wrap",
+        }}
+      >
+        {features.map((feature, index) => (
+          <View
+            key={index}
+            style={{
+              width: isMobile ? "100%" : "23%",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              source={feature.icon}
+              style={{ width: 250, height: 250, marginBottom: 0}}
+              resizeMode="contain"
+            />
+            <Text
+              style={{
+                fontSize: 18,
+                fontFamily: "Loubag-Bold",
+                textAlign: "center",
+                marginBottom: 10,
+                color: "#0D47A1",
+              }}
+            >
+              {feature.title}
+            </Text>
+            <Text
+              style={{
+                fontSize: 14,
+                fontFamily: "Loubag-Regular",
+                textAlign: "center",
+                color: "#0D47A1",
+              }}
+            >
+              {feature.desc}
+            </Text>
+          </View>
+        ))}
+      </View>
+    </View>
+    
+
+      </ScrollView>
+    </View>
   );
 }
