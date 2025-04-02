23fe08765d5f6a75eecf98450d8199bfab3f08c3
Javier Santos <javsanmar5@alum.us.es>
2025-04-02 21:37:28 +0200

    feat: Fetch market items from backend Refs #288

--- frontend/app/(tabs)/marketplace/index.tsx
+++ frontend/app/(tabs)/marketplace/index.tsx
@@ -1,16 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import { ScrollView, View } from 'react-native';
-import { marketItems as initialMarketItems } from '../../../hardcoded_data/marketItems'; // adjust the path as needed
 import { MarketItem } from '../../../types';
 import MarketItemComponent from '../../../components/MarketItem';
+import { useAuth } from '../../../context/AuthContext';
 
 export default function Marketplace() {
-  const [marketItems, setMarketItems] = useState<MarketItem[]>([]);
+  const apiUrl = process.env.EXPO_PUBLIC_API_URL;
   const gs = require("../../../static/styles/globalStyles");
+  const { token } = useAuth();
+
+  const [marketItems, setMarketItems] = useState<MarketItem[]>([]);
 
   useEffect(() => {
-    // Populate the state with the imported market items
-    setMarketItems(initialMarketItems);
+    const fetchMarketItems = async (): Promise<boolean> => {
+      try {
+        const response = await fetch(`${apiUrl}/api/v1/products`, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Error fetching subscription");
+        } 
+
+        const data = await response.json();
+        setMarketItems(data.content);
+        return true;
+
+      } catch (error) {
+        console.error('Error fetching recipes: ', error);
+        return false;
+      }
+    };
+
+    fetchMarketItems();
   }, []);
 
   return (
