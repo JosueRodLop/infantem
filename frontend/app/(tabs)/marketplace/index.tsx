c0f062b34f0555b04b402576368986de48faeedb
Javier Santos <javsanmar5@alum.us.es>
2025-04-03 12:34:44 +0200

    feat: Add pagination component

--- frontend/app/(tabs)/marketplace/index.tsx
+++ frontend/app/(tabs)/marketplace/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { MarketItem } from '../../../types';
 import MarketItemComponent from '../../../components/MarketItem';
 import { useAuth } from '../../../context/AuthContext';
+import Pagination from '../../../components/Pagination';
 
 export default function Marketplace() {
   const apiUrl = process.env.EXPO_PUBLIC_API_URL;
@@ -10,11 +11,14 @@ export default function Marketplace() {
   const { token } = useAuth();
 
   const [marketItems, setMarketItems] = useState<MarketItem[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchMarketItems = async (): Promise<boolean> => {
       try {
-        const response = await fetch(`${apiUrl}/api/v1/products`, {
+        // The backend is zero-indexed. Thats the reason behind the -1
+        const response = await fetch(`${apiUrl}/api/v1/products?page=${page-1}`, {
           method: 'GET',
           headers: {
             'Authorization': `Bearer ${token}`,
@@ -27,6 +31,7 @@ export default function Marketplace() {
 
         const data = await response.json();
         setMarketItems(data.content);
+        setTotalPages(data.totalPages)
         return true;
 
       } catch (error) {
@@ -36,17 +41,27 @@ export default function Marketplace() {
     };
 
     fetchMarketItems();
-  }, []);
+  }, [page]);
 
   return (
     <View style={gs.container}>
       <Text style={{ color: "#1565C0", fontSize: 36, fontWeight: "bold", textAlign: "center", marginBottom: 10 }}>Tienda</Text>
-        <Text style={[gs.bodyText, { textAlign: "center",color:"#1565C0" }]}>Compra todos lo que necesites para tu bebé</Text>
+      <Text style={[gs.bodyText, { textAlign: "center",color:"#1565C0" }]}>Compra todos lo que necesites para tu bebé</Text>
+
       <ScrollView>
         {marketItems.map((item, index) => (
           <MarketItemComponent key={index} item={item} />
         ))}
       </ScrollView>
+
+      {totalPages && (
+        <Pagination 
+          totalPages={totalPages} 
+          page={page} 
+          setPage={setPage} 
+        />
+      )}
+
     </View>
   );
 }
---
+++ frontend/components/Pagination.tsx
@@ -0,0 +1,33 @@
+import { Text, TouchableOpacity, View } from 'react-native';
+
+type PaginationProps = {
+  totalPages: number;
+  page: number;
+  setPage: (page: number) => void;
+};
+
+const Pagination: React.FC<PaginationProps> = ({ totalPages, page, setPage }) => {
+  return (
+    <View style={{ flexDirection: 'row', justifyContent: 'center', margin: 10 }}>
+      {Array.from({ length: totalPages }).map((_, index) => {
+        const num = index + 1; // We want to show an 1-indexed array 
+        return (
+          <TouchableOpacity key={num} onPress={() => setPage(num)}>
+            <View
+              style={{
+                padding: 10,
+                margin: 5,
+                backgroundColor: num === page ? "#007AFF" : 'lightgray',
+                borderRadius: 5,
+              }}
+            >
+              <Text style={{ color: num === page ? 'white' : 'black' }}>{num}</Text>
+            </View>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
+export default Pagination;
