a3417aa91b6238e92479a9ed12849378438f583c
Miguel Galán Lerate <miggaller@alum.us.es>
2025-04-08 12:17:24 +0200

    fix: remove Food and Nutrient entities

--- src/main/java/com/isppG8/infantem/infantem/nutrient/NutrientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.isppG8.infantem.infantem.nutrient;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NutrientRepository extends JpaRepository<Nutrient, Long> {
-}
